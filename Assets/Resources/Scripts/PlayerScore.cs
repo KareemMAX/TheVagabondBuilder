5b21ec110a3f6f4d5a4874f6f878889c53afc5df
Kareem Morsy <kreem.morsy@hotmail.com>
2020-02-01 21:30:39 +0200

    added UI

--- Assets/Resources/Scripts/PlayerScore.cs
+++ Assets/Resources/Scripts/PlayerScore.cs
@@ -1,17 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
 {
     [SerializeField] float blackCoin;
     [SerializeField] float goldHammer;
+    [SerializeField] float totalHammers;
     [SerializeField] float bridge;
 
+    public Text hammerText;
+    public Text blackCoinsText;
+    public Text bridgesText;
+    private void Start()
+    {
+        bridgesText.text = bridge.ToString();
+        blackCoinsText.text = blackCoin.ToString();
+        hammerText.text = string.Format("{0}/{1}", goldHammer, totalHammers);
 
-    public float BlackCoin { get { return blackCoin; } set { blackCoin = value; }  }
-    public float GoldHammer { get { return goldHammer; } set { goldHammer = value; } }
-    public float Bridge { get { return bridge; } set { bridge = value; } }
+    }
+    public float BlackCoin
+    {
+        get
+        {
+            return blackCoin;
+        }
+        set
+        {
+            blackCoin = value;
+            blackCoinsText.text = value.ToString();
+        }
+    }
+
+    public float GoldHammer
+    {
+        get
+        {
+            return goldHammer;
+        }
+        set
+        {
+            goldHammer = value;
+            hammerText.text = string.Format("{0}/{1}", value, totalHammers);
+        }
+    }
+
+    public float TotalHammer
+    {
+        get
+        {
+            return totalHammers;
+        }
+        set
+        {
+            totalHammers = value;
+        }
+    }
+
+    public float Bridge
+    {
+        get
+        {
+            return bridge;
+        }
+        set
+        {
+            bridge = value;
+            bridgesText.text = value.ToString();
+        }
+    }
 
 
 }
