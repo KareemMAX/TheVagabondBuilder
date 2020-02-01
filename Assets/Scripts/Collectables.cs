08fb86c44246b901f32817e5bd719442d8bb289a
Kareem Morsy <kreem.morsy@hotmail.com>
2020-02-01 19:14:22 +0200

    Collectales progress

---
+++ Assets/Scripts/Collectables.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    public float rotationSpeed = 50f;
+    public float hoverDis = 0.02f;
+    public float hoverSpeed = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, Mathf.Sin(transform.eulerAngles.y * hoverSpeed) * hoverDis, 0);
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Collide");
+        Debug.Log(collider.gameObject.tag);
+        if(collider.gameObject.tag == "Player")
+        {
+            animator.SetBool("Dead", true);
+        }
+    }
+}
